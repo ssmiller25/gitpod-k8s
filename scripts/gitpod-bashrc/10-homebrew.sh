3a69c287a399c248924404847648887b21ea8215
Steve Miller <me@r15cookie.com>
2022-03-26 13:16:36 +0000

    Adding homebrew to env environment

---
+++ scripts/gitpod-bashrc/10-homebrew.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Gitpod bashrc - may replace with my containered "dotfiles" later at some point...but later...
+
+# Include linuxbrew paths as parimary path in containers
+eval "$(homebrew/bin/brew shellenv)"
